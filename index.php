58380e23610c30167ff3aaa0693614504057e9bc
sanjibg <sanjibg@hp.com>
2014-07-19 13:01:04 -0700

    Update index.php

--- index.php
+++ index.php
@@ -17,8 +17,7 @@
 </head>
 <body>
 	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<h2><?php echo "This was made by Sanjib's son, Shovik";?></h2>
+	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; <br> echo "This was made by Shovik";?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
